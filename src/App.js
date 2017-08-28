3149ed4dd6671674c0748c936a5f49781148b5f5
karthikpamidimarri <karthikpamidimari@gmail.com>
2017-08-29 00:43:53 +0530

    Set the component name whatever we want using static method displayane for readability and debugging purposes
    App.displayName = "Whateverwewant"

--- src/App.js
+++ src/App.js
@@ -3,8 +3,11 @@ import React, { Component } from 'react'
 class App extends Component {
   render() {
     return (
-<h1>Hello</h1>    )
+      <h1>Hello</h1>
+    )
   }
 }
+
+App.displayName = "WhateverWeWant"
 
 export default App
