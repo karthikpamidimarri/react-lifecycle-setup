97c4b21c15abf2ac6a031889987b0e8320e59d2a
karthikpamidimarri <karthikpamidimari@gmail.com>
2017-08-29 00:17:57 +0530

    A simple example for HOC

--- src/App.js
+++ src/App.js
@@ -1,10 +1,38 @@
 import React, { Component } from 'react'
 
 class App extends Component {
+
+  static displayName = "Something Name"
+
+  static defaultProps = {
+    someImportArray: []
+  }
+
   render() {
+      console.log(this.props)
     return (
-<h1>Hello</h1>    )
+      <h1>Hello</h1>
+    )
   }
 }
+
+function myTestWrapper(WrappedComponent) {
+
+  return class extends Component{
+    render() {
+      console.log(this.props)
+      return (
+          <div
+            style={{backgroundColor: "blue"}}
+          >
+            <WrappedComponent/>
+          </div>
+      )
+    }
+  }
+}
+
+App = myTestWrapper(App)
+
 
 export default App
